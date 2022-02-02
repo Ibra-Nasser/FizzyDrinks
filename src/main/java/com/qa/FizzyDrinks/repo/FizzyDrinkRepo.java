e73b282bd06d604a7ec600504d6103c037b50702
ibraheem nasser <ibraheemalialkaby@gmail.com>
2022-02-02 21:15:11 +0000

    created repo

---
+++ src/main/java/com/qa/FizzyDrinks/repo/FizzyDrinkRepo.java
@@ -0,0 +1,11 @@
+package com.qa.FizzyDrinks.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.FizzyDrinks.domain.FizzyDrink;
+
+@Repository
+public interface FizzyDrinkRepo extends JpaRepository<FizzyDrink, Integer> {
+
+}
