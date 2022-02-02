a6d74da858e671202457614b582cf2511c1462f0
ibraheem nasser <ibraheemalialkaby@gmail.com>
2022-02-02 20:08:33 +0000

    initial spring repo

---
+++ src/main/java/com/qa/FizzyDrinks/FizzyDrinksApplication.java
@@ -0,0 +1,13 @@
+package com.qa.FizzyDrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzyDrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzyDrinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/FizzyDrinks/FizzyDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.FizzyDrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzyDrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
