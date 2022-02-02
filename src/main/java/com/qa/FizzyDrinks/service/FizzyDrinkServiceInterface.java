cc5c1c2418beb6bc8da368d574191b8f62010804
ibraheem nasser <ibraheemalialkaby@gmail.com>
2022-02-02 22:03:11 +0000

    added service interface and completed with create, read all, read by id, update and delete

--- src/main/java/com/qa/FizzyDrinks/service/FizzyDrinkServiceInterface.java
+++ src/main/java/com/qa/FizzyDrinks/service/FizzyDrinkServiceInterface.java
@@ -1,5 +1,21 @@
 package com.qa.FizzyDrinks.service;
 
-public interface FizzyDrinkServiceInterface {
+import java.util.List;
+
+public interface FizzyDrinkServiceInterface<T> {
+	// Create Object
+	T createFizzyDrink(T t);
+
+	// Read All
+	List<T> getFizzyDrink();
+
+	// Read by ID
+	T getFizzyDrinkById(Integer Id);
+
+	// Update by Object ID
+	T updateFizzyDrinkById(T t, Integer Id);
+
+	// Delete - return boolean
+	boolean removeFizzyDrink(Integer Id);
 
 }
